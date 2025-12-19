8347b4c481fc4aad58d610a936a0419aee32e4a9
Anisse Bendrihem <anisse.bendrihem@gmail.com>
2025-12-19 21:40:08 +0100

    feat: check cone for shadow

--- src/light/shadow.c
+++ src/light/shadow.c
@@ -3,36 +3,33 @@
 /*                                                        :::      ::::::::   */
 /*   shadow.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: esouhail <souhailelhoussain@gmail.com>     +#+  +:+       +#+        */
+/*   By: abendrih <abendrih@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/18 03:20:26 by esouhail          #+#    #+#             */
-/*   Updated: 2025/12/19 20:29:14 by esouhail         ###   ########.fr       */
+/*   Updated: 2025/12/19 21:39:17 by abendrih         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
-#include "vector_math.h"
 #include "ray.h"
+#include "vector_math.h"
 
-int is_in_shadow(t_scene *scene, t_light *light, t_vec3 hit_point, t_vec3 normal)
+int	is_in_shadow(t_scene *scene, t_light *light, t_vec3 hit_point,
+		t_vec3 normal)
 {
-	t_ray   shadow_ray;
-	t_vec3  light_dir;
-	double  dist_to_light;
-	t_hit   shadow_hit;
-	t_list  *node;
+	t_ray	shadow_ray;
+	t_vec3	light_dir;
+	double	dist_to_light;
+	t_hit	shadow_hit;
+	t_list	*node;
 
 	light_dir = vec_sub(light->pos, hit_point);
 	dist_to_light = vec_len(light_dir);
-	
 	light_dir = vec_normalize(light_dir);
-
 	// Offset to aviod acne dots
 	shadow_ray.origin = vec_add(hit_point, vec_scale(0.001, normal));
 	shadow_ray.direction = light_dir;
-	
 	shadow_hit.t = dist_to_light;
-
 	// Check spheres
 	node = scene->spheres;
 	while (node)
@@ -42,7 +39,6 @@ int is_in_shadow(t_scene *scene, t_light *light, t_vec3 hit_point, t_vec3 normal
 				return (1);
 		node = node->next;
 	}
-
 	// Check planes
 	node = scene->planes;
 	while (node)
@@ -52,7 +48,6 @@ int is_in_shadow(t_scene *scene, t_light *light, t_vec3 hit_point, t_vec3 normal
 				return (1);
 		node = node->next;
 	}
-
 	// Check cylinders
 	node = scene->cylinders;
 	while (node)
@@ -65,6 +60,14 @@ int is_in_shadow(t_scene *scene, t_light *light, t_vec3 hit_point, t_vec3 normal
 				return (1);
 		node = node->next;
 	}
-
+	// Check cones
+	node = scene->cones;
+	while (node)
+	{
+		if (hit_cone(shadow_ray, (t_cone *)node->obj, &shadow_hit))
+			if (shadow_hit.t < dist_to_light)
+				return (1);
+		node = node->next;
+	}
 	return (0);
 }
